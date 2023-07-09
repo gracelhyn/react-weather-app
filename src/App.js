a97cb57610bcdd24d4e4c1ca36ca0c5a4fbfe74d
Jiel <legasteji.el@gmail.com>
2023-07-09 20:45:15 +0800

    added github link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
           <div className="wrapper mt-5 p-5">
             <SearchWeather />
           </div>
+          <div className="container">
+            <p>
+              This site is coded by Grace Lhyn Legaste and is{" "}
+              <a href="https://github.com/gracelhyn/react-weather-app">
+                Open-source
+              </a>
+            </p>
+          </div>
         </div>
       </header>
     </div>
