fba990530b4b73a2d180f5ce4690d63b04c5f64b
Jiel <legasteji.el@gmail.com>
2023-07-09 12:49:49 +0800

    Updated ShowWeather to display data from API

--- src/ShowWeather.js
+++ src/ShowWeather.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function ShowWeather(props) {
+  let [name, setName] = useState();
+  let [country, setCountry] = useState();
+  let [temperature, setTemperature] = useState(0);
+  let [description, setDescription] = useState();
+  let [humidity, setHumidity] = useState();
+  let [wind, setWind] = useState();
+
+  function handleResponse(response) {
+    // console.log(response.data);
+    setName(response.data.name);
+    setCountry(response.data.sys.country);
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+  }
+  let apiKey = "7746bdeabca928cfedcad71e52fd9d66";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <>
       <div className="row">
         <div className="col-7 current-weather">
           <h1 id="city" className="city">
-            {props.cityName}, Philippines
+            {name}, {country}
           </h1>
           <h3 id="description" className="description">
-            Broken clouds
+            {description}
           </h3>
         </div>
         <div className="col-8">
@@ -21,7 +40,7 @@ export default function ShowWeather(props) {
             id="weather-icon"
           />
           <span className="temperature" id="temperature">
-            0
+            {Math.round(temperature)}
           </span>
           <span className="temp-unit">
             <a href="/" id="celcius" className="active">
@@ -35,8 +54,8 @@ export default function ShowWeather(props) {
         </div>
         <div className="col-4">
           <ul className="weather-details">
-            <li id="humidity">Humidity: 10%</li>
-            <li id="wind">Wind: 3km/h</li>
+            <li id="humidity">Humidity: {Math.round(humidity)}%</li>
+            <li id="wind">Wind: {Math.round(wind)}km/h</li>
           </ul>
         </div>
       </div>
